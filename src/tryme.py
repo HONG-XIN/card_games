bf45bcb277d26bc8745e818ab84294497e89b5a9
dezhouw <hongsinwang@gmail.com>
2019-04-04 21:55:57 -0400

    main menu interface almost complete

--- src/tryme.py
+++ src/tryme.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtGui import QColor
 from PyQt5.QtGui import QFont
@@ -11,6 +12,27 @@ from PyQt5.QtGui import QBrush
 from PyQt5.QtGui import QRadialGradient
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+
+StyleSheet = '''
+QPushButton {
+	min-width: 200px;
+	max-width: 200px;
+	min-height: 36px;
+	max-height: 36px;	
+}
+
+QPushButton#exit {
+	min-width: 100px;
+	max-width: 100px;
+	min-height: 36px;
+	max-height: 36px;	
+}
+
+
+QPushButton:hover {
+	background-color: #ffffff;
+}
+'''
 
 class App(QWidget):
 
@@ -41,16 +63,36 @@ class App(QWidget):
 		pic.setAlignment(Qt.AlignCenter)
 		pic.setPixmap(QPixmap("../res/images/menu_title.png"))
 
+		# Set menu left-bottom decoration
+		dec = QLabel(self)
+		dec.setGeometry(20, 520, 600, 500)
+		dec.setPixmap(QPixmap("../res/images/menu_decoration.png"))
+
+		# Set Blackjack game button
+		blackjack_button = QPushButton('Blackjack')
+		# blackjack_button.clicked.connect()
+		
+		# Set Texas Hold'em game button
+		texas_holdem_button = QPushButton('Texas Hold\'em')
+		# texas_holdem_button.clicked.connect()
+
+		# Add game buttons to layout
+		buttons_layout = QVBoxLayout(self)
+		buttons_layout.setAlignment(Qt.AlignCenter)
+		buttons_layout.addWidget(blackjack_button)
+		buttons_layout.addWidget(texas_holdem_button)
+
 		# Exit button
 		exit_button = QPushButton('EXIT', self)
+		exit_button.setObjectName('exit')
 		exit_button.clicked.connect(self.close)
-		exit_button.resize(100, 36)
 		exit_button.move(410, 780)
 		
 		self.show()
 
 def main():
 	app = QApplication(sys.argv)
+	app.setStyleSheet(StyleSheet)
 	ex = App()
 	sys.exit(app.exec_())
 
